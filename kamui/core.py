908848932d87d537941b26fd3ff7dfd0bae903a3
Chin-Yun Yu <chin-yun.yu@qmul.ac.uk>
2024-04-05 08:38:02 +0000

    Refactor weight calculation in puma function

--- kamui/core.py
+++ kamui/core.py
@@ -200,16 +200,21 @@ def puma(psi: np.ndarray, edges: np.ndarray, max_jump: int = 1, p: float = 1):
 
             G.add_edges(edges[:, 0], edges[:, 1], weight, np.zeros_like(weight))
 
+            a = e10 - e00
+            flip_mask = a < 0
             tmp_st_weight = np.zeros((2, total_nodes))
+            flip_index = np.stack((flip_mask.astype(np.int), 1 - flip_mask.astype(np.int)), axis=1)
+            positive_a = np.where(flip_mask, -a, a)
+            np.add.at(tmp_st_weight, (flip_index.ravel(), edges.ravel()), positive_a.repeat(2))
 
-            for i, a in enumerate(e10 - e00):
-                u, v = edges[i]
-                if a > 0:
-                    tmp_st_weight[0, u] += a
-                    tmp_st_weight[1, v] += a
-                else:
-                    tmp_st_weight[1, u] -= a
-                    tmp_st_weight[0, v] -= a
+            # for i, a in enumerate(e10 - e00):
+            #     u, v = edges[i]
+            #     if a > 0:
+            #         tmp_st_weight[0, u] += a
+            #         tmp_st_weight[1, v] += a
+            #     else:
+            #         tmp_st_weight[1, u] -= a
+            #         tmp_st_weight[0, v] -= a
 
             for i in range(total_nodes):
                 G.add_tedge(i, tmp_st_weight[0, i], tmp_st_weight[1, i])
